616fa1cd233dc5e780516757f0702a85d7d1fe62
James Collins <james_c.collins@tufts.edu>
2014-05-15 15:17:38 -0400

    adding script editing html

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+	var object = document.getElementById('embedded_object');
+	object.style.width = "100%";
+	object.style.height = "600px";
+	object.style.border = "1pt solid #000000";
+	var swf = document.getElementById('swffile');
+	swf.style.width = "100%";
+	swf.style.height = "100%";
+}
