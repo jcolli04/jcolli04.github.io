2e6fbce64b6f7f04b5efb1899644b07c1debc1df
James Collins <james_c.collins@tufts.edu>
2014-05-15 13:54:10 -0400

    changing script

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+	document.body.width = "100%";
+	document.body.height = "100%";
 	var object = document.getElementById('embedded_object')
 	object.style.width = "100%";
 	object.style.height = "100%";
