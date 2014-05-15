28d8ca095b9748f4b15e5984a2bff0002e67542c
James Collins <james_c.collins@tufts.edu>
2014-05-15 15:39:20 -0400

    changed script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function () {
 	var object = document.getElementById('embedded_object');
 	object.style.width = "100%";
 	console.log(window.innerHeight);
-	object.style.height = window.innerHeight;
+	object.style.height = window.innerHeight+'px';
 	object.style.border = "1pt solid #000000";
 	var swf = document.getElementById('swffile');
 	swf.style.width = "100%";
